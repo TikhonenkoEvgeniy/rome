0ff579fd47619707e4c4daaf8cfc004c63423dbc
evgeniytikhonenko <tihonenko87@gmail.com>
2022-11-22 23:27:38 +0700

    first commit2

--- src/java/Main.java
+++ src/java/Main.java
@@ -17,21 +17,23 @@ public class Main {
 
     private static String convert(int x) {
         int[] numbers = new int[] {1000, 500, 100, 50, 10, 5, 1};
+        int[] numbers2 = new int[] {900, 400, 90, 40, 9, 4};
         String[] romes = new String[] {"M", "D", "C", "L", "X", "V", "I"};
+        String[] romes2 = new String[] {"DM", "CD", "XC", "XL", "IX", "IV"};
         StringBuilder result = new StringBuilder();
-
         for (int i = 0; i < numbers.length; i++) {
-            if (x < numbers[i]) continue;
-
-
-
+            if (x == 0) break;
+            if (x < numbers[i]) {
+                if (x >= numbers2[i]) {
+                    x = x - numbers2[i];
+                    result.append(romes2[i]);
+                }
+                continue;
+            }
             int temp = x / numbers[i];
             result.append(repeat(romes[i], temp));
             x = x - temp * numbers[i];
-
-            if (x == 0) break;
         }
-
         return result.toString();
     }
 
